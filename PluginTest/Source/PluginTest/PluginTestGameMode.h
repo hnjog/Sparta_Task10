d273885b0e3c8b56a6ec66fdded52e8609f0b0be
Hyun <hnjogo@gmail.com>
2025-12-05 16:12:24 +0900

    [Add] Start Project

---
+++ PluginTest/Source/PluginTest/PluginTestGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PluginTestGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APluginTestGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APluginTestGameMode();
+};
+
+
+
